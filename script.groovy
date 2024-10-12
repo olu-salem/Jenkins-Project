2ab1edbefd04c204d16dbc8fc9a42d58be579da5
Olusegun <olusegun.salem@gmail.com>
2024-10-12 12:30:54 -0500

    editted docker build command

--- script.groovy
+++ script.groovy
@@ -15,7 +15,7 @@ def incrementVersion() {
 
 def buildImage() {
     echo "building docker image..."
-    withCredentials([usernamePassword(credentialsId: 'Dockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker_credential', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh "docker build -t ${IMAGE_NAME} ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
         sh "docker push ${IMAGE_NAME}"
@@ -23,12 +23,14 @@ def buildImage() {
 }
 
 def deployAppOnEC2() {
-    def shellCmd="bash ./shell-cmd.sh ${IMAGE_NAME}"
-    def ec2_instance = ${EC2_PUBLIC_IP}
-    sshagent(['ec2-instance']) {
-    sh "scp -o StrictHostKeyChecking=no shell-cmd.sh ${ec2_instance}:/home/ec2-user"
-    sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2_instance}:/home/ec2-user"
-    sh "ssh -o StrictHostKeyChecking=no ${ec2_instance} ${shellCmd}"
+   // def shellCmd="bash ./shell-cmd.sh ${IMAGE_NAME}"
+    // def ec2_instance = ${EC2_PUBLIC_IP}
+    def dockercmd = "docker run -p 8080:8080 -d ${IMAGE_NAME}"
+    sshagent(['appServerCredential']) {
+   // sh "scp -o StrictHostKeyChecking=no shell-cmd.sh ${ec2_instance}:/home/ec2-user"
+  //  sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2_instance}:/home/ec2-user"
+ //   sh "ssh -o StrictHostKeyChecking=no ${ec2_instance} ${shellCmd}"
+        sh "ssh -o StrictHostKeyChecking=no ec2-user@${EC2_PUBLIC_IP} ${dockercmd}"
     }
 }
 
